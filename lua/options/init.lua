1ea4eb25fedfb8da2e141f00bf0ba6a26c267eab
nindgabeet <wenming.soh@gmail.com>
2025-10-11 16:16:14 +0800

    saving config, before trying to move keymaps

--- lua/options/init.lua
+++ lua/options/init.lua
@@ -79,3 +79,6 @@ vim.o.scrolloff = 10
 -- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
 vim.o.confirm = true
+vim.opt.colorcolumn = '80'
+vim.o.termguicolors = true
+vim.api.nvim_set_hl(0, 'ColorColumn', { bg = '#ff0000' })

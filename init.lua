4818b456c79f8d15718d460090bf75f10846a9b8
nindgabeet <wenming.soh@gmail.com>
2025-10-11 16:34:36 +0800

    moved installation of lazy package manager

--- init.lua
+++ init.lua
@@ -23,33 +23,12 @@ If you experience any errors while trying to install kickstart, run `:checkhealt
 require 'options'
 -- gets the keymaps module - ~/.config/nvim/lua/keymaps/
 require 'keymaps'
--- gets the autocommand module - ~/.config/nvim/lua/keymaps/
+-- gets the autocommand module - ~/.config/nvim/lua/autocommands/
 require 'autocommands'
-
--- [[ Install `lazy.nvim` plugin manager ]]
---    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-  if vim.v.shell_error ~= 0 then
-    error('Error cloning lazy.nvim:\n' .. out)
-  end
-end
-
----@type vim.Option
-local rtp = vim.opt.rtp
-rtp:prepend(lazypath)
-
--- [[ Configure and install plugins ]]
---
+-- installation of lazy.nvim package manager - ~/.config/nvim/lua/lazy_package/
 --  To check the current status of your plugins, run
 --    :Lazy
---
---  You can press `?` in this menu for help. Use `:q` to close the window
---
---  To update plugins you can run
---    :Lazy update
+require 'lazy_package'
 
 -- plugins install start
 -- NOTE: Here is where you install your plugins.
